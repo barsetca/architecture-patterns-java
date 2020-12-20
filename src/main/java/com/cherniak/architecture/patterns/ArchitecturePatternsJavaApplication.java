ef3933d81d0527b67d1426bac2bc59449c1c6209
Viktor Cherniak <barsetca@gmail.com>
2020-12-20 11:47:12 +0300

    Initial commit

---
+++ src/main/java/com/cherniak/architecture/patterns/ArchitecturePatternsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.cherniak.architecture.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturePatternsJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArchitecturePatternsJavaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/cherniak/architecture/patterns/ArchitecturePatternsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherniak.architecture.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecturePatternsJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
