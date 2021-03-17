172773702c38743f968ed7d36bd6ea9cb8823da0
Viktor Cherniak <barsetca@gmail.com>
2021-03-18 01:06:52 +0300

    master after rebase

--- src/main/java/com/cherniak/architecture/patterns/rebase/Main.java
+++ src/main/java/com/cherniak/architecture/patterns/rebase/Main.java
@@ -5,6 +5,8 @@ public class Main {
   public static void main(String[] args) {
     User user = new User(1, "name", "mail");
     System.out.println(user.setDefaultConcat());
+    System.out.println(user.setDefaultName());
+    System.out.println(user.setDefaultEmail());
   }
 
 }
