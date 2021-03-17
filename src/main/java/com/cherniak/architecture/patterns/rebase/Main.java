e75181cd2ba02896618f6e89a7e3e5ee23778198
Viktor Cherniak <barsetca@gmail.com>
2021-03-18 01:14:55 +0300

    feature comment

--- src/main/java/com/cherniak/architecture/patterns/rebase/Main.java
+++ src/main/java/com/cherniak/architecture/patterns/rebase/Main.java
@@ -3,12 +3,11 @@ package com.cherniak.architecture.patterns.rebase;
 public class Main {
 
   public static void main(String[] args) {
-    User user = new User(2, "name2", "mail2");
+    User user = new User(1, "name", "mail");
     System.out.println(user.setDefaultConcat());
     System.out.println(user.setDefaultName());
     System.out.println(user.setDefaultEmail());
-    System.out.println("Rebase");
-   // var registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    // то нибудь новенькое
   }
 
 }
