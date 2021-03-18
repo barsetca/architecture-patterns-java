710f9b24f6770c062aa669094627d5dfe81a69e2
Viktor Cherniak <barsetca@gmail.com>
2021-03-18 09:05:14 +0300

    master change user

--- src/main/java/com/cherniak/architecture/patterns/rebase/Main.java
+++ src/main/java/com/cherniak/architecture/patterns/rebase/Main.java
@@ -3,7 +3,7 @@ package com.cherniak.architecture.patterns.rebase;
 public class Main {
 
   public static void main(String[] args) {
-    User user = new User(1, "name", "mail");
+    User user = new User(2, "name2", "mail2");
     System.out.println(user.setDefaultConcat());
     System.out.println(user.setDefaultName());
     //System.out.println(user.setDefaultEmail());
